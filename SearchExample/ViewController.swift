4784c5e5804bb40f3c9f138b1a80a71af38d820e
osanaikoutarou <osanaikoutarou@gmail.cpm>
2020-11-12 13:47:46 +0900

    Initial Commit

---
+++ SearchExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchExample
+//
+//  Created by 長内幸太郎 on 2020/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
