839d264551a397fd66bb09357d16d3f06fe218af
osanaikoutarou <osanaikoutarou@gmail.cpm>
2020-11-12 14:27:00 +0900

    init

---
+++ SearchExample/SearchResultsController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchResultsController.swift
+//  SearchExample
+//
+//  Created by 長内幸太郎 on 2020/11/12.
+//
+
+import UIKit
+
+class SearchResultsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SearchExample/ViewController.swift
+++ SearchExample/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//参考: https://dev.classmethod.jp/articles/ios-11-uinavigationitem-searchcontroller/
 
+class ViewController: UIViewController {
+    
+    // ナビゲーションバーに検索を出すやつ
+    private var searchController: UISearchController!
+    // 検索結果を随時表示する画面
+    private var searchResultsController: SearchResultsController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Storyboardから読み込む
+        searchResultsController = self.storyboard?.instantiateViewController(identifier: "SearchResultsController") as? SearchResultsController
+        
+        // SearchControllerを設定する
+        searchController = UISearchController(searchResultsController: searchResultsController)
+        searchController.hidesNavigationBarDuringPresentation = true
+        searchController.obscuresBackgroundDuringPresentation = false
+        // 検索結果updater
+        searchController.searchResultsUpdater = self
+
+        // テキストバーの見た目をカスタマイズする
+//        searchController.searchBar.searchTextField.backgroundColor = .lightGray
+        searchController.searchBar.searchTextField.textColor = .darkGray
+        searchController.searchBar.searchTextField.font = UIFont.systemFont(ofSize: 15)
+        searchController.searchBar.placeholder = "検索"
+ 
+        // これを設定しないと崩れが生じる
+        definesPresentationContext = true
+
+        // navigationItemに反映
+        navigationItem.searchController = searchController
+        navigationItem.title = "タイトル"
+        // trueだとスクロールした時にSearchBarを隠す（デフォルトはtrue）
+        navigationItem.hidesSearchBarWhenScrolling = false
     }
+}
 
-
+extension ViewController: UISearchResultsUpdating {
+    // 検索のTextFieldの内容が変化した時に呼ばれる
+    func updateSearchResults(for searchController: UISearchController) {
+        if let keyword = searchController.searchBar.text, !keyword.isEmpty {
+            print(#line, keyword)
+        }
+        else {
+            // 検索結果画面を表示する
+            self.searchController.showsSearchResultsController = true
+        }
+    }
 }
 
